96c442bc2ebf715fa4f9186449a6c8d81130f89c
KIZUKU <kizu@MacBookAir.local>
2020-07-11 16:57:20 +0900

    [Add] success,error message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    @books = Book.all.order(created_at: :asc)
     @book = Book.new
   end
 
@@ -11,13 +11,15 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-  end
-
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      @books = Book.all.order(created_at: :asc)
+      render :index
+    end
   end
 
   def edit
@@ -25,14 +27,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = 'Book was successfully destroyed.'
     redirect_to books_path
   end
 
